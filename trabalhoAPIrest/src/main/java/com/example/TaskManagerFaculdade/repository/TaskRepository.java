93a585953b40e82e8dd649ad3be26ba505372357
Fabricio Gomes <66139608+bijass@users.noreply.github.com>
2025-05-26 16:15:48 -0300

    Delete trabalhoAPIrest directory

--- trabalhoAPIrest/src/main/java/com/example/TaskManagerFaculdade/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.TaskManagerFaculdade.repository;
-
-import com.example.TaskManagerFaculdade.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
